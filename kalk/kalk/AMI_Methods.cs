9a7c1a162df8567f612a3f94c750d85a8543acc2
Jakub Kowalik <kjunag@wp.pl>
2021-05-13 01:11:35 +0200

    added AMI

---
+++ kalk/kalk/AMI_Methods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kalk
+{
+    class AMI_Methods
+    {
+        public static string ami_encode(string input)
+        {
+            int i = 0;
+            char last_one = '-';
+            var code = new StringBuilder();
+            while (i <= input.Length - 1)
+            {
+                if (input[i] == '1')
+                {
+                    if (last_one == '-')
+                    {
+                        code.Append("+");
+                        last_one = '+';
+                    }
+                    else
+                    {
+                        code.Append("-");
+                        last_one = '-';
+                    }
+                }
+                else
+                {
+                    code.Append("0");
+                }
+                i++;
+            }
+            return code.ToString();
+        }
+    }
+}
---
+++ kalk/kalk/Views/AMI_page.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace kalk.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AMI_page : ContentPage
+    {
+        public AMI_page()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_encode_Clicked(object sender, EventArgs e)
+        {
+            
+            ami_data_out.Text = AMI_Methods.ami_encode(ami_data_in.Text);
+        }
+    }
+}
